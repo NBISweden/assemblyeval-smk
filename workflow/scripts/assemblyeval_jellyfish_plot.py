5f8770ff15551d97a436e01e5bbd6e4105eabef2
Per Unneberg <per.unneberg@scilifelab.se>
2021-02-18 21:43:55 +0100

    Rewrite kmer plot to depend on custom jellyfish c-program

--- workflow/scripts/assemblyeval_jellyfish_plot.py
+++ workflow/scripts/assemblyeval_jellyfish_plot.py
@@ -1,52 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import dna_jellyfish as jf
-import numpy as np
+import sys
+import shutil
 import seaborn as sns
-from tqdm import tqdm
 import pandas as pd
+from snakemake.shell import shell
+from snakemake.utils import logger
 
-assembly = snakemake.input.assembly
-reads = snakemake.input.reads
+asm_file = snakemake.input.assembly
+read_file = snakemake.input.reads
+tsv_file = snakemake.output.tsv
 
+exe = "kmer_count_pairs"
+if shutil.which(exe) is None:
+    logger.error(
+        "[assemblyeval_jellyfish_plot]: no such binary '{exe}'; install in PATH"
+    )
+    sys.exit(1)
 
-def load_kmers(fn):
-    data = {}
-    cmax = 0
-    mf = jf.ReadMerFile(fn)
-    for mer, count in tqdm(mf):
-        data[str(mer)] = count
-        cmax = max(cmax, count)
-    return cmax, data
+# FIXME: kmer_count_pairs must be built automatically
+shell("{exe} {asm_file} {read_file} > {tsv_file}")
 
+with open(tsv_file, "r") as fh:
+    df = pd.read_csv(fh, sep="\t")
 
-cmax1, read_data = load_kmers(reads)
-cmax2, assembly_data = load_kmers(assembly)
-cmax2 = min(cmax2, 6)
+df.columns = ["asm_mers", "read_mers", "count"]
+cmax1 = min(max(df["asm_mers"]), 6)
+cmax2 = max(df["read_mers"])
+df["asm_mers"] = df["asm_mers"].apply(lambda x: x if x < cmax1 else cmax1)
 
-m = np.zeros((cmax1 + 1, cmax2 + 1))
+llab = [f"{x}X" for x in set(df["asm_mers"])]
+llab[-1] = f"{llab[-1]}+"
 
-for mer, count in tqdm(assembly_data.items()):
-    if count >= m.shape[1]:
-        count = m.shape[1] - 1
-    if str(mer) in read_data.keys():
-        readcount = read_data[str(mer)]
-        m[readcount][count] = m[readcount][count] + 1
-    else:
-        m[0][count] = m[0][count] + 1
-
-for mer, count in tqdm(read_data.items()):
-    if str(mer) not in assembly_data.items():
-        m[count][0] = m[count][0] + 1
-
-df = pd.DataFrame(m).stack().to_frame().reset_index()
-df.columns = ["kcount", "kdistinct", "val"]
-cmax = max(df["kdistinct"])
-df["kdistinct"] = df["kdistinct"].astype("str").replace({f"{cmax}": f"{cmax}+"})
-
-p = sns.displot(df, x="kcount", weights="val", hue="kdistinct", kind="kde")
+p = sns.lineplot(data=df, x="read_mers", y="count", hue="asm_mers")
 p.set(xlim=(0, None), title="k-mer comparison plot")
-p.set_axis_labels("k-mer multiplicity", "Number of distinct k-mers")
+p.set_xlabel("read k-mer multiplicity")
+p.set_ylabel("Number of distinct k-mers")
+p.legend(llab, title="Assembly k-mer multiplicity")
 
 p.savefig(snakemake.output.png)
-df.to_csv(snakemake.output.tsv, index=False, sep="\t")
