2745cfd0684a44f27a07a9b29d7f16801cd14cf8
Per Unneberg <per.unneberg@scilifelab.se>
2021-03-24 10:34:19 +0100

    Make single fasta chunks
    Eliminates the problem of having to know beforehand how many
    partitions are required.

--- workflow/scripts/assemblyeval_pybedtools_make_chunks.py
+++ workflow/scripts/assemblyeval_pybedtools_make_chunks.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 import re
-from multiprocessing import Pool
 import pybedtools
 from tqdm import tqdm
 from snakemake.shell import shell
@@ -11,6 +10,8 @@ from snakemake.utils import logger
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 faidx = snakemake.input.faidx
+partition = int(snakemake.wildcards.partition)
+analysis = snakemake.wildcards.analysis
 
 regions = []
 with open(faidx) as fh:
@@ -19,7 +20,8 @@ with open(faidx) as fh:
         i = pybedtools.Interval(fields[0], 0, int(fields[1]))
         regions.append(i)
 
-npart = len(snakemake.output)
+npart = snakemake.params.npartitions
+
 try:
     assert len(regions) >= npart
 except AssertionError as e:
@@ -27,6 +29,16 @@ except AssertionError as e:
         (
             f"Number of regions smaller than number of partitions: '{len(regions)} < {npart}': "
             f"lower the number of partitions "
+        )
+    )
+    raise
+
+try:
+    assert partition < npart
+except AssertionError as e:
+    logger.error(
+        (
+            f"partition number {partition} larger than the maximum number of partitions {npart}"
         )
     )
     raise
@@ -39,15 +51,7 @@ for j in tqdm(range(npart, len(regions))):
     out[imin].append(regions[j])
     outlen[imin] += len(regions[j])
 
-
-def _getfasta(args):
-    i, out, output, inseq = args
-    logger.info(f"saving chunk {i} to {output}...")
-    bed = pybedtools.BedTool(out)
-    logger.info(f"reading input {inseq}")
-    bed.sequence(fi=inseq).save_seqs(output)
-
-logger.info(f"Saving {len(out)} chunks...")
-args = [(i, out[i], snakemake.output[i], snakemake.input.seq) for i in range(len(out))]
-with Pool(snakemake.threads) as p:
-    list(p.imap(_getfasta, args))
+logger.info(f"saving chunk {partition} to {snakemake.output.fasta}...")
+bed = pybedtools.BedTool(out[partition])
+logger.info(f"reading input {snakemake.input.seq}")
+bed.sequence(fi=snakemake.input.seq).save_seqs(snakemake.output.fasta)
